d4a3bd6e52e03e41cb3d5ba8d6109d3f39c40776
Manuchekhr Raupov <82057826+manuchekhr32@users.noreply.github.com>
2021-12-05 20:56:51 +0500

    Delete animate.js

--- animate.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-    let h1 = document.getElementById("h1");
-    let cm = document.getElementById("cm");
-
-    setTimeout(() => {
-        h1.classList = "animate__animated animate__zoomInDown h1"
-        cm.classList = "animate__animated animate__flipInX ph"
-    }, 500)
-})
